b49de924642179efda7f7a7e838b89b19797e2be
Dohee Kim <43026521+goodmary1121@users.noreply.github.com>
2023-03-22 11:06:10 +0900

    Update get-data.sh

--- get-data.sh
+++ get-data.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-mkdir -p corpus
+# mkdir -p corpus
 # download KFTT
-wget http://www.phontron.com/kftt/download/kftt-data-1.0.tar.gz
-tar xzvf kftt-data-1.0.tar.gz
-rm kftt-data-1.0.tar.gz
-mv kftt-data-1.0 ./corpus
+# wget http://www.phontron.com/kftt/download/kftt-data-1.0.tar.gz
+# tar xzvf kftt-data-1.0.tar.gz
+# rm kftt-data-1.0.tar.gz
+# mv kftt-data-1.0 ./corpus
 
 # download sentencepiece model
 wget http://www.kecl.ntt.co.jp/icl/lirg/jparacrawl/release/1.0/spm_models/en-ja_spm.tar.gz
